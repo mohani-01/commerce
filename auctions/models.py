d4b729cfeae85398e1443b59a9383a06e0682c8f
Nahom <nahomohani@gmail.com>
2023-08-03 06:43:26 +0300

    First commit

---
+++ auctions/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    pass
+
+class Bid(models.Model):
+    pass
+
+class Comments(model.Model):
+    pass
